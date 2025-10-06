3d4db3efa39027f8172746afb2cb291fa8e4de84
knwpound <pound.somparn@gmail.com>
2025-10-06 19:16:17 +0700

    Create useAos.tsx

---
+++ src/้hooks/useAos.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+export const useAOS = (duration: number = 1000) => {
+  useEffect(() => {
+    AOS.init({ duration });
+  }, [duration]);
+};
