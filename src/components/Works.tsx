810355003432ac8b5647d88bcb525a43d3526a18
knwpound <pound.somparn@gmail.com>
2025-10-06 20:57:26 +0700

    Update Works.tsx

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image"
-import AutoScrollGallery from "./AutoScrollGallery"
+import Image from "next/image";
+import AutoScrollGallery from "./AutoScrollGallery";
 export function Works() {
-  return <div className="w-full h-screen flex flex-col justify-center gap-10">
-    <h1 className="text-3xl ms-20 font-semibold">Explore My Work</h1>
-    
-<AutoScrollGallery/>
+  return (
+    <div className="w-full h-screen flex flex-col justify-center gap-10"
+    style={{
+          background:
+            "radial-gradient(circle at 90% 10%, #CAD183, #ffff, #ffff)",
+          zIndex: 0,
+        }}>
+      <h1 className="text-3xl ms-20 font-semibold">Explore My Work</h1>
 
-    
-  </div>
+      <AutoScrollGallery />
+    </div>
+  );
 }
