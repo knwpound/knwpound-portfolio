15613512635ed66e800eae9efeb150f5a9951eb3
knwpound <pound.somparn@gmail.com>
2026-01-08 13:03:47 +0700

    Update Capstone.tsx

--- src/components/Capstone.tsx
+++ src/components/Capstone.tsx
@@ -8,7 +8,7 @@ export function Capstone() {
       data-aos="fade-up"
       className="w-full h-screen flex flex-col justify-center bg-yellow-400 px-[5%]"
     >
-      <h1 className="text-3xl font-semibold">I'm currently working on..</h1>
+      <h1 className="text-3xl font-semibold">I have experience in ...</h1>
       <div className="flex flex-row justify-center gap-40 mt-10">
         <div 
           className="flex flex-col items-center w-[200px] h-[200px] bg-white shadow-md py-4 px-2 gap-2 -rotate-5 
@@ -18,9 +18,9 @@ export function Capstone() {
             <div className="bg-red-400 w-[20px] h-[20px] relative top-3.5 start-1 z-20 rounded-full"></div>
             <div className="bg-red-600 w-[20px] h-[20px] relative z-10 rounded-full"></div>
           </div>
-          <h1 className="text-center text-3xl font-semibold">August</h1>
+          <h1 className="text-center text-3xl font-semibold">Research</h1>
           <p className="w-full text-center text-sm font-light">
-            Requirement gathering: Interview Stakeholder & Website Auditing
+            User Interview , Related-product Auditing, User Persona & User Journey 
           </p>
         </div>
         <div
@@ -31,9 +31,9 @@ export function Capstone() {
             <div className="bg-red-400 w-[20px] h-[20px] relative top-3.5 start-1 z-20 rounded-full"></div>
             <div className="bg-red-600 w-[20px] h-[20px] relative z-10 rounded-full"></div>
           </div>
-          <h1 className="text-center text-3xl font-semibold">September</h1>
+          <h1 className="text-center text-3xl font-semibold">UI Skills</h1>
           <p className="w-full text-center text-sm font-light">
-            Sketching whole website
+            Wireframing (Low & High Fidelity), Design Systems, Basic Design Knowledge
           </p>
         </div>
         <div
@@ -44,9 +44,10 @@ export function Capstone() {
             <div className="bg-red-400 w-[20px] h-[20px] relative top-3.5 start-1 z-20 rounded-full"></div>
             <div className="bg-red-600 w-[20px] h-[20px] relative z-10 rounded-full"></div>
           </div>
-          <h1 className="text-center text-3xl font-semibold">October</h1>
+          <h1 className="text-center text-3xl font-semibold">Figma</h1>
           <p className="w-full text-center text-sm font-light">
-            Design System & Wireframe
+            Design System & Design Token
+Interactive Prototyping : Auto Layout, Components, Variants
           </p>
         </div>
       </div>
@@ -59,9 +60,9 @@ export function Capstone() {
             <div className="bg-red-400 w-[20px] h-[20px] relative top-3.5 start-1 z-20 rounded-full"></div>
             <div className="bg-red-600 w-[20px] h-[20px] relative z-10 rounded-full"></div>
           </div>
-          <h1 className="text-center text-3xl font-semibold">November</h1>
+          <h1 className="text-center text-3xl font-semibold">Testing</h1>
           <p className="w-full text-center text-sm font-light">
-            Testing & Iteration
+            Iterative design process including User Testing with Feedback
           </p>
         </div>
         <div
@@ -72,9 +73,9 @@ export function Capstone() {
             <div className="bg-red-400 w-[20px] h-[20px] relative top-3.5 start-1 z-20 rounded-full"></div>
             <div className="bg-red-600 w-[20px] h-[20px] relative z-10 rounded-full"></div>
           </div>
-          <h1 className="text-center text-3xl font-semibold">Final</h1>
+          <h1 className="text-center text-3xl font-semibold">Coding</h1>
           <p className="w-full text-center text-sm font-light">
-            New Computer Engineering Website
+            Intermediate Software Developer with internship experience
           </p>
         </div>
       </div>
