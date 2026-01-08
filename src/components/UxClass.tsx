2a16e849551723c151b624dcb85ae7f0341fd70a
knwpound <pound.somparn@gmail.com>
2026-01-08 14:25:01 +0700

    Update UxClass.tsx

--- src/components/UxClass.tsx
+++ src/components/UxClass.tsx
@@ -1,28 +1,58 @@
 "use client";
+import Link from "next/link";
 import { Files } from "./File";
 import { useAOS } from "@/้hooks/useAos";
 export function UxClass() {
   useAOS(1000);
   return (
     <div className="w-full h-screen bg-[#FF8D4B] flex flex-col py-10 justify-center items-center">
-      <h1 className="text-3xl font-semibold">I'm also taking UxUi Classes</h1>
       <div
         data-aos="fade-down"
         data-aos-duration="1000"
-        className="mt-45 animate-fall"
+        className="mt-30 animate-fall"
       >
         <div
-          className={`absolute flex flex-col w-[500px] h-[200px] bg-[#EECE7B] hover:-translate-y-32 transition-transform duration-400 z-40`}
+          className={`absolute flex flex-col w-[500px] h-[200px] bg-[#EECE7B] hover:-translate-y-40 transition-transform duration-400 z-40`}
         >
           <div
             className={`absolute -top-7 ms-0 w-[125px] h-[30px] bg-[#EECE7B] rounded-t-lg cursor-pointer`}
           ></div>
-          <div className="w-full h-full flex flex-col gap-2 px-4 py-5">
-            <h1 className="text-2xl font-semibold">Requirement Gathering</h1>
-            <div className="w-full flex flex-col gap-0">
-              <p className="w-full">1. How to filter target users?</p>
-              <p className="w-full">2. How to correctly interview users?</p>
-              <p className="w-full">3. Ask LOT of "Why" Questions</p>
+          <div className="w-full h-full flex flex-row gap-2 px-4 py-3">
+            <div className="w-full h-full flex flex-col gap-1">
+              <div className="w-full flex flex-col">
+                <div className="w-full flex flex-row justify-between">
+                  <h1 className="text-xl font-semibold">CP Website</h1>
+                  <Link
+                    href={
+                      "https://www.figma.com/design/omfIdKjQSJmBEcNpgwRctD/Wireframe?node-id=27-53&t=Px2REJiCr3qt6rRr-1"
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button
+                      className="bg-black text-white border-1 border-black text-sm font-semibold px-3 py-1 rounded-md
+                  hover:bg-white hover:text-black transition-transform duration-300"
+                    >
+                      Figma
+                    </button>
+                  </Link>
+                </div>
+
+                <p className="text-xs">Senior Project – Website Redesign </p>
+              </div>
+
+              <p className="text-sm text-justify">
+                The process included{" "}
+                <b className="font-semibold">Requirement Gathering</b> through{" "}
+                <i>surveys</i> and <i>interviews</i>, followed by Website{" "}
+                <i>Auditing</i> and related research. Low-Fidelity
+                <b className="font-semibold"> Wireframes</b> were sketched and
+                refined with feedback, then translated into a consistent{" "}
+                <b className="font-semibold">Design System</b> and High-Fidelity
+                Wireframes, culminating in a{" "}
+                <b className="font-semibold">Clickable Prototype</b> tested with
+                users.
+              </p>
             </div>
           </div>
         </div>
@@ -33,40 +63,99 @@ export function UxClass() {
             className={`absolute -top-7 ms-[125px] w-[125px] h-[30px] bg-[#7EA763] rounded-t-lg cursor-pointer`}
           ></div>
           <div className="w-full h-full flex flex-col gap-2 px-4 py-5">
-            <h1 className="text-2xl font-semibold">Design Thinking</h1>
-            <p className="w-full">
-              Brainstome the existing problems that affect specific target
-            </p>
+            <h1 className="text-2xl font-semibold">To be filled ...</h1>
           </div>
         </div>
         <div
-          className={`absolute flex flex-col w-[500px] h-[200px] bg-[#DA5577] hover:-translate-y-32 transition-transform duration-400 z-20`}
+          className={`absolute flex flex-col w-[500px] h-[200px] bg-[#DA5577] hover:-translate-y-40 transition-transform duration-400 z-20`}
         >
           <div
             className={`absolute -top-7 ms-[250px] w-[125px] h-[30px] bg-[#DA5577] rounded-t-lg cursor-pointer`}
           ></div>
-          <div className="w-full h-full flex flex-col gap-2 px-4 py-5">
-            <h1 className="text-2xl font-semibold">Design</h1>
-            <p className="w-full">
-              Create Clickable Prototype for usability testing
-            </p>
+          <div className="w-full h-full flex flex-col gap-2 px-4 py-3">
+            <div className="w-full h-full flex flex-col gap-1">
+              <div className="w-full flex flex-col">
+                <div className="w-full flex flex-row justify-between">
+                  <h1 className="text-xl font-semibold">Fitness</h1>
+                  <Link
+                    href={
+                      "https://www.figma.com/design/LrfaZHiF1lie1aAPwezPzn/Fitness-App?node-id=0-1&t=IFnhHE5eiWnnPP7r-1"
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button
+                      className="bg-black text-white border-1 border-black text-sm font-semibold px-3 py-1 rounded-md
+                  hover:bg-white hover:text-black transition-transform duration-300"
+                    >
+                      Figma
+                    </button>
+                  </Link>
+                </div>
+
+                <p className="text-xs">
+                  Course Project – Fitness application for non-trainer users
+                </p>
+              </div>
+
+              <p className="text-sm text-justify">
+                The process covered{" "}
+                <b className="font-semibold">Requirement Gathering</b>,
+                <b className="font-semibold">Application Auditing</b>, and
+                creating{" "}
+                <b className="font-semibold">Low-Fidelity Wireframes</b>
+                with feedback, followed by a{" "}
+                <b className="font-semibold">Design System</b>,{" "}
+                <b className="font-semibold">High-Fidelity Wireframes</b>, and a{" "}
+                <b className="font-semibold">Clickable Prototype</b> tested with
+                users.
+              </p>
+            </div>
           </div>
         </div>
         <div
-          className={`absolute flex flex-col w-[500px] h-[200px] bg-[#E5D0FF] hover:-translate-y-36 transition-transform duration-400 z-20`}
+          className={`absolute flex flex-col w-[500px] h-[200px] bg-[#E5D0FF] hover:-translate-y-40 transition-transform duration-400 z-20`}
         >
           <div
             className={`absolute -top-7 ms-[375px] w-[125px] h-[30px] bg-[#E5D0FF] rounded-t-lg cursor-pointer`}
           ></div>
           <div className="w-full h-full flex flex-col items-end gap-2 px-4 py-5">
-            <h1 className="text-2xl font-semibold">Usability Testing</h1>
-            <div className="w-full flex flex-col gap-0">
-              <p className="w-full text-end">Give Objective</p>
-              <p className="w-full text-end">
-                Let user find their own way to finish
-              </p>
-              <p className="w-full text-end">
-                Monitoring their behavior and errors 
+            <div className="w-full h-full flex flex-col gap-1">
+              <div className="w-full flex flex-col items-end">
+                <div className="w-full flex flex-row justify-between">
+                  <Link
+                    href={
+                      "https://www.figma.com/design/Y2eAMZ9AV4BRHSSOB1o0JI/PPY-Website-Design?node-id=219-513&t=fECyz2HAs1UaWzOM-1"
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <button
+                      className="bg-black text-white border-1 border-black text-sm font-semibold px-3 py-1 rounded-md
+                  hover:bg-white hover:text-black transition-transform duration-300"
+                    >
+                      Figma
+                    </button>
+                  </Link>
+                  <h1 className="text-xl font-semibold">PPY</h1>
+                </div>
+
+                <p className="text-xs">
+                  Side Project – Redesign of Plookpanya School website
+                </p>
+              </div>
+
+              <p className="text-sm text-justify">
+                The process covered{" "}
+                <b className="font-semibold">Requirement Gathering</b>,{" "}
+                <b className="font-semibold">Application Auditing</b>, and
+                creating{" "}
+                <b className="font-semibold">Low-Fidelity Wireframes</b> with
+                feedback, followed by a{" "}
+                <b className="font-semibold">Design System</b>,{" "}
+                <b className="font-semibold">High-Fidelity Wireframes</b>, and a{" "}
+                <b className="font-semibold">Clickable Prototype</b> tested with
+                users.
               </p>
             </div>
           </div>
@@ -74,7 +163,7 @@ export function UxClass() {
         <div className="relative top-4 flex flex-col items-center pt-6 w-[500px] h-[250px] bg-white rounded-md z-50">
           <div className="w-[50%] h-[30px] bg-[#EECE7B] rounded-full"></div>
           <div className="absolute flex justify-center items-center w-[50%] h-[20%] bottom-10 pt-2 border-10 border-gray-200">
-            <h1 className="font-apple">Lecture</h1>
+            <h1 className="font-apple">Portfolio</h1>
           </div>
         </div>
       </div>
